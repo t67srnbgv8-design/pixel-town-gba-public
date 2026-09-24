300b7c6db2da66876eee0f699c685debd09fcda7
t67srnbgv8-design <t67srnbgv8@privaterelay.appleid.com>
2026-09-25 00:37:41 +0200

    Update world.h

--- source/world.h
+++ source/world.h
@@ -1,33 +1,15 @@
-#ifndef WORLD_H
-#define WORLD_H
+#ifndef PIXELTOWN_WORLD_H
+#define PIXELTOWN_WORLD_H
 
 #include <gba.h>
 
-#define MAP_W 64
-#define MAP_H 32
+#define META_MAP_W 32
+#define META_MAP_H 16
 
-#define WORLD_W (MAP_W * 8)
-#define WORLD_H (MAP_H * 8)
+#define WORLD_W (META_MAP_W * 16)
+#define WORLD_H (META_MAP_H * 16)
 
-typedef struct
-{
-    u16 ground;
-    u16 upper;
-    u8 collision;
-    u8 elevation;
-    u8 behavior;
-} MapTile;
-
-enum
-{
-    BEHAVIOR_NORMAL = 0,
-    BEHAVIOR_WATER,
-    BEHAVIOR_DOOR,
-    BEHAVIOR_GRASS,
-    BEHAVIOR_SIGN
-};
-
-extern MapTile world[MAP_H][MAP_W];
+extern u16 worldMap[META_MAP_H][META_MAP_W];
 
 void worldInit(void);
 void worldDraw(void);

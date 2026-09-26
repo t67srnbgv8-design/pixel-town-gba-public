51aad4fd9e189b8babaa04bbfc4f99e8f129fa88
t67srnbgv8-design <t67srnbgv8@privaterelay.appleid.com>
2026-09-26 18:19:30 +0200

    Update character_select.c

--- source/character_select.c
+++ source/character_select.c
@@ -1,631 +1,378 @@
 #include <gba.h>
-
 #include "character_select.h"
 
-#define SCREEN_W 240
-#define SCREEN_H 160
+#define W 240
+#define H 160
 
-#define COLOR_BG       RGB5(5,10,18)
-#define COLOR_PANEL    RGB5(10,16,24)
-#define COLOR_WHITE    RGB5(31,31,31)
-#define COLOR_GREY     RGB5(18,20,22)
-#define COLOR_BLUE     RGB5(7,17,31)
-#define COLOR_GOLD     RGB5(31,23,7)
-#define COLOR_SKIN     RGB5(27,18,13)
-#define COLOR_DARKSKIN RGB5(18,11,7)
-#define COLOR_BLACK    RGB5(3,3,4)
-#define COLOR_BROWN    RGB5(12,6,3)
-#define COLOR_BLONDE   RGB5(28,22,10)
-#define COLOR_REDHAIR  RGB5(21,7,3)
-#define COLOR_RED      RGB5(26,5,5)
-#define COLOR_GREEN    RGB5(6,23,9)
-#define COLOR_PURPLE   RGB5(18,7,25)
+#define C_BG      RGB5(5,10,18)
+#define C_PANEL   RGB5(10,16,24)
+#define C_WHITE   RGB5(31,31,31)
+#define C_GREY    RGB5(17,19,22)
+#define C_GOLD    RGB5(31,23,7)
+#define C_SKIN    RGB5(27,18,13)
+#define C_BLACK   RGB5(3,3,4)
+#define C_BROWN   RGB5(12,6,3)
+#define C_BLONDE  RGB5(28,22,10)
+#define C_REDHAIR RGB5(21,7,3)
+#define C_BLUE    RGB5(5,14,29)
+#define C_RED     RGB5(26,5,5)
+#define C_GREEN   RGB5(6,22,9)
+#define C_PURPLE  RGB5(18,7,25)
+#define C_DARK    RGB5(6,7,10)
 
 CharacterConfig characterConfig =
 {
-    0,
-    1,
-    0,
-    0
+    0,  /* gender */
+    1,  /* hairColor */
+    0,  /* hairLength */
+    0   /* clothes */
 };
 
-static volatile u16 *video =
-    (volatile u16 *)MODE3_FB;
+static volatile u16 *fb = (volatile u16 *)MODE3_FB;
 
-static void pixel(
-    int x,
-    int y,
-    u16 color
-)
+/* =========================================================
+   DRAW
+   ========================================================= */
+
+static void px(int x, int y, u16 c)
 {
-    if (
-        x < 0 ||
-        y < 0 ||
-        x >= SCREEN_W ||
-        y >= SCREEN_H
-    )
+    if (x < 0 || y < 0 || x >= W || y >= H)
         return;
 
-    video[y * SCREEN_W + x] = color;
+    fb[y * W + x] = c;
 }
 
-static void rect(
-    int x,
-    int y,
-    int w,
-    int h,
-    u16 color
-)
+static void box(int x, int y, int w, int h, u16 c)
 {
     int xx;
     int yy;
 
     for (yy = 0; yy < h; yy++)
-    {
         for (xx = 0; xx < w; xx++)
-        {
-            pixel(
-                x + xx,
-                y + yy,
-                color
-            );
-        }
-    }
-}
-
-static void clearScreen(u16 color)
-{
-    rect(
-        0,
-        0,
-        SCREEN_W,
-        SCREEN_H,
-        color
-    );
+            px(x + xx, y + yy, c);
 }
 
 /* =========================================================
-   SMALL PIXEL FONT
-
-   Only characters required by this menu.
+   FONT
    ========================================================= */
 
 static const unsigned char font[][7] =
 {
-    {14,17,17,31,17,17,17}, /* A */
-    {30,17,17,30,17,17,30}, /* B */
-    {14,17,16,16,16,17,14}, /* C */
-    {30,17,17,17,17,17,30}, /* D */
-    {31,16,16,30,16,16,31}, /* E */
-    {31,16,16,30,16,16,16}, /* F */
-    {14,17,16,23,17,17,14}, /* G */
-    {17,17,17,31,17,17,17}, /* H */
-    {14,4,4,4,4,4,14},      /* I */
-    {7,2,2,2,18,18,12},     /* J */
-    {17,18,20,24,20,18,17}, /* K */
-    {16,16,16,16,16,16,31}, /* L */
-    {17,27,21,21,17,17,17}, /* M */
-    {17,25,21,19,17,17,17}, /* N */
-    {14,17,17,17,17,17,14}, /* O */
-    {30,17,17,30,16,16,16}, /* P */
-    {14,17,17,17,21,18,13}, /* Q */
-    {30,17,17,30,20,18,17}, /* R */
-    {15,16,16,14,1,1,30},   /* S */
-    {31,4,4,4,4,4,4},       /* T */
-    {17,17,17,17,17,17,14}, /* U */
-    {17,17,17,17,17,10,4},  /* V */
-    {17,17,17,21,21,21,10}, /* W */
-    {17,17,10,4,10,17,17},  /* X */
-    {17,17,10,4,4,4,4},     /* Y */
-    {31,1,2,4,8,16,31}      /* Z */
+    {14,17,17,31,17,17,17},
+    {30,17,17,30,17,17,30},
+    {14,17,16,16,16,17,14},
+    {30,17,17,17,17,17,30},
+    {31,16,16,30,16,16,31},
+    {31,16,16,30,16,16,16},
+    {14,17,16,23,17,17,14},
+    {17,17,17,31,17,17,17},
+    {14,4,4,4,4,4,14},
+    {7,2,2,2,18,18,12},
+    {17,18,20,24,20,18,17},
+    {16,16,16,16,16,16,31},
+    {17,27,21,21,17,17,17},
+    {17,25,21,19,17,17,17},
+    {14,17,17,17,17,17,14},
+    {30,17,17,30,16,16,16},
+    {14,17,17,17,21,18,13},
+    {30,17,17,30,20,18,17},
+    {15,16,16,14,1,1,30},
+    {31,4,4,4,4,4,4},
+    {17,17,17,17,17,17,14},
+    {17,17,17,17,17,10,4},
+    {17,17,17,21,21,21,10},
+    {17,17,10,4,10,17,17},
+    {17,17,10,4,4,4,4},
+    {31,1,2,4,8,16,31}
 };
 
-static void drawChar(
-    int x,
-    int y,
-    char c,
-    u16 color
-)
+static void chr(int x, int y, char c, u16 color)
 {
-    int row;
+    int r;
     int col;
-    int index;
+    int n;
 
     if (c < 'A' || c > 'Z')
         return;
 
-    index =
-        c - 'A';
+    n = c - 'A';
 
-    for (row = 0; row < 7; row++)
+    for (r = 0; r < 7; r++)
     {
         for (col = 0; col < 5; col++)
         {
-            if (
-                font[index][row] &
-                (1 << (4 - col))
-            )
-            {
-                pixel(
-                    x + col,
-                    y + row,
-                    color
-                );
-            }
+            if (font[n][r] & (1 << (4 - col)))
+                px(x + col, y + r, color);
         }
     }
 }
 
-static void text(
-    int x,
-    int y,
-    const char *str,
-    u16 color
-)
+static void txt(int x, int y, const char *s, u16 color)
 {
-    while (*str)
+    while (*s)
     {
-        if (*str == ' ')
-        {
-            x += 6;
-        }
-        else
-        {
-            drawChar(
-                x,
-                y,
-                *str,
-                color
-            );
+        if (*s != ' ')
+            chr(x, y, *s, color);
 
-            x += 6;
-        }
-
-        str++;
+        x += 6;
+        s++;
     }
 }
 
 /* =========================================================
-   CHARACTER PREVIEW
+   APPEARANCE
    ========================================================= */
 
-static u16 hairColor(void)
+static u16 hair(void)
 {
     switch (characterConfig.hairColor)
     {
-        case 0:
-            return COLOR_BLACK;
-
-        case 1:
-            return COLOR_BROWN;
-
-        case 2:
-            return COLOR_BLONDE;
-
-        default:
-            return COLOR_REDHAIR;
+        case 0: return C_BLACK;
+        case 1: return C_BROWN;
+        case 2: return C_BLONDE;
+        default: return C_REDHAIR;
     }
 }
 
-static u16 shirtColor(void)
+static u16 clothesColor(void)
 {
     switch (characterConfig.clothes)
     {
-        case 0:
-            return COLOR_BLUE;
-
-        case 1:
-            return COLOR_RED;
-
-        case 2:
-            return COLOR_GREEN;
-
-        default:
-            return COLOR_PURPLE;
+        case 0: return C_BLUE;
+        case 1: return C_RED;
+        case 2: return C_GREEN;
+        default: return C_PURPLE;
     }
 }
 
-static void drawPreview(void)
+static const char *genderName(void)
 {
-    int x = 178;
-    int y = 45;
+    return characterConfig.gender == 0
+        ? "MAN"
+        : "WOMAN";
+}
 
-    u16 hair =
-        hairColor();
+static const char *hairColorName(void)
+{
+    switch (characterConfig.hairColor)
+    {
+        case 0: return "BLACK";
+        case 1: return "BROWN";
+        case 2: return "BLOND";
+        default: return "RED";
+    }
+}
 
-    u16 shirt =
-        shirtColor();
+static const char *hairLengthName(void)
+{
+    return characterConfig.hairLength == 0
+        ? "SHORT"
+        : "LONG";
+}
 
-    /*
-        Shadow
-    */
+static const char *clothesName(void)
+{
+    switch (characterConfig.clothes)
+    {
+        case 0: return "TSHIRT";
+        case 1: return "HOODIE";
+        case 2: return "JACKET";
+        default: return "SWEATER";
+    }
+}
 
-    rect(
-        x - 10,
-        y + 69,
-        20,
-        3,
-        COLOR_BLACK
-    );
+/* =========================================================
+   BETTER GBA-RPG PREVIEW
+   ========================================================= */
 
-    /*
-        Legs
-    */
+static void preview(void)
+{
+    int x = 185;
+    int y = 48;
 
-    rect(
-        x - 7,
-        y + 49,
-        5,
-        18,
-        COLOR_GREY
-    );
+    u16 hc = hair();
+    u16 shirt = clothesColor();
 
-    rect(
-        x + 2,
-        y + 49,
-        5,
-        18,
-        COLOR_GREY
-    );
+    /* shadow */
+    box(x - 12, y + 67, 24, 4, C_DARK);
+    box(x - 9, y + 65, 18, 3, C_DARK);
 
-    /*
-        Shoes
-    */
+    /* shoes */
+    box(x - 9, y + 59, 8, 7, C_BLACK);
+    box(x + 1, y + 59, 8, 7, C_BLACK);
 
-    rect(
-        x - 8,
-        y + 65,
-        7,
-        4,
-        COLOR_BLACK
-    );
+    /* trousers / legs */
+    box(x - 7, y + 46, 6, 15, C_DARK);
+    box(x + 1, y + 46, 6, 15, C_DARK);
 
-    rect(
-        x + 1,
-        y + 65,
-        7,
-        4,
-        COLOR_BLACK
-    );
-
-    /*
-        Body
-
-        Female option has slightly narrower shoulders.
-    */
-
+    /* torso silhouette */
     if (characterConfig.gender == 0)
     {
-        rect(
-            x - 9,
-            y + 29,
-            18,
-            22,
-            shirt
-        );
-
-        rect(
-            x - 13,
-            y + 31,
-            4,
-            17,
-            COLOR_SKIN
-        );
-
-        rect(
-            x + 9,
-            y + 31,
-            4,
-            17,
-            COLOR_SKIN
-        );
+        box(x - 11, y + 27, 22, 21, shirt);
+        box(x - 14, y + 30, 4, 15, C_SKIN);
+        box(x + 10, y + 30, 4, 15, C_SKIN);
     }
     else
     {
-        rect(
-            x - 8,
-            y + 29,
-            16,
-            22,
-            shirt
-        );
-
-        rect(
-            x - 12,
-            y + 32,
-            4,
-            15,
-            COLOR_SKIN
-        );
-
-        rect(
-            x + 8,
-            y + 32,
-            4,
-            15,
-            COLOR_SKIN
-        );
+        box(x - 9, y + 27, 18, 19, shirt);
+        box(x - 11, y + 42, 22, 6, shirt);
+        box(x - 13, y + 30, 4, 14, C_SKIN);
+        box(x + 9, y + 30, 4, 14, C_SKIN);
     }
 
-    /*
-        Neck
-    */
-
-    rect(
-        x - 3,
-        y + 25,
-        6,
-        6,
-        COLOR_SKIN
-    );
-
-    /*
-        Head
-    */
-
-    rect(
-        x - 9,
-        y + 8,
-        18,
-        18,
-        COLOR_SKIN
-    );
-
-    /*
-        ears
-    */
-
-    rect(
-        x - 11,
-        y + 14,
-        2,
-        7,
-        COLOR_SKIN
-    );
-
-    rect(
-        x + 9,
-        y + 14,
-        2,
-        7,
-        COLOR_SKIN
-    );
-
-    /*
-        Hair top
-    */
-
-    rect(
-        x - 10,
-        y + 5,
-        20,
-        7,
-        hair
-    );
-
-    rect(
-        x - 9,
-        y + 10,
-        4,
-        7,
-        hair
-    );
-
-    rect(
-        x + 5,
-        y + 10,
-        4,
-        5,
-        hair
-    );
-
-    /*
-        Hair length
-    */
-
-    if (characterConfig.hairLength == 1)
+    /* actual clothing silhouettes */
+    if (characterConfig.clothes == 1)
     {
-        rect(
-            x - 10,
-            y + 12,
-            4,
-            18,
-            hair
-        );
-
-        rect(
-            x + 6,
-            y + 12,
-            4,
-            18,
-            hair
-        );
+        /* hoodie */
+        box(x - 12, y + 25, 24, 6, shirt);
+        box(x - 8, y + 43, 16, 3, C_DARK);
+    }
+    else if (characterConfig.clothes == 2)
+    {
+        /* jacket */
+        box(x - 2, y + 27, 4, 21, C_DARK);
+        box(x - 9, y + 29, 3, 14, C_WHITE);
+        box(x + 6, y + 29, 3, 14, C_WHITE);
+    }
+    else if (characterConfig.clothes == 3)
+    {
+        /* sweater */
+        box(x - 12, y + 29, 3, 16, shirt);
+        box(x + 9, y + 29, 3, 16, shirt);
+        box(x - 8, y + 44, 16, 3, C_DARK);
+    }
+    else
+    {
+        /* t-shirt sleeves */
+        box(x - 13, y + 28, 5, 8, shirt);
+        box(x + 8, y + 28, 5, 8, shirt);
     }
 
-    /*
-        eyes
-    */
+    /* neck */
+    box(x - 3, y + 23, 6, 6, C_SKIN);
 
-    pixel(
-        x - 4,
-        y + 17,
-        COLOR_BLACK
-    );
+    /* head – larger GBA RPG proportions */
+    box(x - 9, y + 7, 18, 17, C_SKIN);
+    box(x - 7, y + 5, 14, 21, C_SKIN);
 
-    pixel(
-        x + 4,
-        y + 17,
-        COLOR_BLACK
-    );
+    /* ears */
+    box(x - 11, y + 12, 3, 8, C_SKIN);
+    box(x + 8, y + 12, 3, 8, C_SKIN);
 
-    /*
-        shirt highlight
-    */
+    /* hair silhouette */
+    box(x - 9, y + 3, 18, 7, hc);
+    box(x - 11, y + 6, 5, 10, hc);
+    box(x + 6, y + 6, 5, 8, hc);
 
-    rect(
-        x - 4,
-        y + 32,
-        3,
-        10,
-        COLOR_WHITE
-    );
+    /* hair crown */
+    box(x - 6, y + 1, 12, 3, hc);
+    px(x - 7, y + 2, hc);
+    px(x + 7, y + 2, hc);
+
+    if (characterConfig.hairLength)
+    {
+        box(x - 11, y + 12, 4, 17, hc);
+        box(x + 7, y + 12, 4, 17, hc);
+        box(x - 8, y + 23, 3, 6, hc);
+        box(x + 5, y + 23, 3, 6, hc);
+    }
+
+    /* eyes */
+    box(x - 5, y + 15, 2, 2, C_BLACK);
+    box(x + 3, y + 15, 2, 2, C_BLACK);
+
+    /* nose/shadow */
+    px(x, y + 19, C_BROWN);
+
+    /* shirt highlight */
+    if (characterConfig.clothes != 2)
+        box(x - 5, y + 30, 2, 9, C_WHITE);
 }
 
 /* =========================================================
    MENU
    ========================================================= */
 
-static void drawArrow(
-    int x,
-    int y
-)
+static void arrow(int x, int y)
 {
-    pixel(x, y, COLOR_GOLD);
-
-    rect(
-        x + 1,
-        y - 1,
-        2,
-        3,
-        COLOR_GOLD
-    );
-
-    rect(
-        x + 3,
-        y - 2,
-        2,
-        5,
-        COLOR_GOLD
-    );
+    px(x, y, C_GOLD);
+    box(x + 1, y - 1, 2, 3, C_GOLD);
+    box(x + 3, y - 2, 2, 5, C_GOLD);
 }
 
-static void drawMenu(
-    int selected
-)
+static void drawMenu(int selected)
 {
-    clearScreen(
-        COLOR_BG
-    );
-
     /*
-        Header
+        IMPORTANT:
+        This is only called while the LCD is hidden.
+        Therefore Delta never sees the screen halfway drawn.
     */
 
-    rect(
-        8,
-        7,
-        224,
-        18,
-        COLOR_PANEL
-    );
+    box(0, 0, W, H, C_BG);
 
-    text(
-        56,
-        12,
-        "CHARACTER",
-        COLOR_WHITE
-    );
+    box(8, 7, 224, 18, C_PANEL);
+    txt(68, 12, "CREATE CHARACTER", C_WHITE);
 
-    /*
-        Menu panel
-    */
+    box(10, 32, 143, 100, C_PANEL);
+    box(158, 32, 72, 100, C_PANEL);
 
-    rect(
-        12,
-        34,
-        133,
-        94,
-        COLOR_PANEL
-    );
+    txt(25, 42, "GENDER", C_WHITE);
+    txt(91, 42, genderName(), C_GOLD);
 
-    text(
-        28,
-        45,
-        "GENDER",
-        COLOR_WHITE
-    );
+    txt(25, 62, "HAIR", C_WHITE);
+    txt(91, 62, hairColorName(), C_GOLD);
 
-    text(
-        28,
-        65,
-        "HAIR COLOR",
-        COLOR_WHITE
-    );
+    txt(25, 82, "LENGTH", C_WHITE);
+    txt(91, 82, hairLengthName(), C_GOLD);
 
-    text(
-        28,
-        85,
-        "HAIR LENGTH",
-        COLOR_WHITE
-    );
+    txt(25, 102, "CLOTHES", C_WHITE);
+    txt(91, 102, clothesName(), C_GOLD);
 
-    text(
-        28,
-        105,
-        "CLOTHES",
-        COLOR_WHITE
-    );
+    arrow(16, 45 + selected * 20);
 
-    drawArrow(
-        18,
-        48 + selected * 20
-    );
+    preview();
 
-    /*
-        Preview panel
-    */
+    txt(15, 143, "DPAD CHANGE", C_GREY);
+    txt(169, 143, "START", C_GOLD);
+}
 
-    rect(
-        153,
-        34,
-        74,
-        94,
-        COLOR_PANEL
-    );
+/*
+    Blank display while changing the Mode 3 framebuffer.
 
-    drawPreview();
+    Bit 7 of DISPCNT = forced blank.
+*/
 
-    /*
-        Bottom
-    */
+static void redrawHidden(int selected)
+{
+    REG_DISPCNT |= 0x0080;
 
-    text(
-        22,
-        141,
-        "LEFT RIGHT CHANGE",
-        COLOR_GREY
-    );
+    drawMenu(selected);
 
-    text(
-        153,
-        141,
-        "START",
-        COLOR_GOLD
-    );
+    VBlankIntrWait();
+
+    REG_DISPCNT &= ~0x0080;
 }
 
 /* =========================================================
-   CHARACTER SELECT LOOP
+   SELECT
    ========================================================= */
 
 void characterSelectRun(void)
 {
     int selected = 0;
-    int dirty = 1;
 
-    SetMode(
-        MODE_3 |
-        BG2_ON
-    );
+    SetMode(MODE_3 | BG2_ON);
+
+    redrawHidden(selected);
 
     while (1)
     {
-        VBlankIntrWait();
+        u16 down;
 
+        VBlankIntrWait();
         scanKeys();
 
-        u16 down =
-            keysDown();
+        down = keysDown();
 
         if (down & KEY_UP)
         {
@@ -634,20 +381,18 @@ void characterSelectRun(void)
             if (selected < 0)
                 selected = 3;
 
-            dirty = 1;
+            redrawHidden(selected);
         }
-
-        if (down & KEY_DOWN)
+        else if (down & KEY_DOWN)
         {
             selected++;
 
             if (selected > 3)
                 selected = 0;
 
-            dirty = 1;
+            redrawHidden(selected);
         }
-
-        if (down & KEY_LEFT)
+        else if (down & KEY_LEFT)
         {
             if (selected == 0)
             {
@@ -657,12 +402,8 @@ void characterSelectRun(void)
             {
                 characterConfig.hairColor--;
 
-                if (
-                    characterConfig.hairColor < 0
-                )
-                {
+                if (characterConfig.hairColor < 0)
                     characterConfig.hairColor = 3;
-                }
             }
             else if (selected == 2)
             {
@@ -672,18 +413,13 @@ void characterSelectRun(void)
             {
                 characterConfig.clothes--;
 
-                if (
-                    characterConfig.clothes < 0
-                )
-                {
+                if (characterConfig.clothes < 0)
                     characterConfig.clothes = 3;
-                }
             }
 
-            dirty = 1;
+            redrawHidden(selected);
         }
-
-        if (down & KEY_RIGHT)
+        else if (down & KEY_RIGHT)
         {
             if (selected == 0)
             {
@@ -693,12 +429,8 @@ void characterSelectRun(void)
             {
                 characterConfig.hairColor++;
 
-                if (
-                    characterConfig.hairColor > 3
-                )
-                {
+                if (characterConfig.hairColor > 3)
                     characterConfig.hairColor = 0;
-                }
             }
             else if (selected == 2)
             {
@@ -708,38 +440,26 @@ void characterSelectRun(void)
             {
                 characterConfig.clothes++;
 
-                if (
-                    characterConfig.clothes > 3
-                )
-                {
+                if (characterConfig.clothes > 3)
                     characterConfig.clothes = 0;
-                }
             }
 
-            dirty = 1;
-        }
-
-        if (dirty)
-        {
-            drawMenu(
-                selected
-            );
-
-            dirty = 0;
+            redrawHidden(selected);
         }
 
         if (down & KEY_START)
             break;
     }
 
-    /*
-        Wait until START is released.
-    */
-
     do
     {
         VBlankIntrWait();
         scanKeys();
+    }
+    while (keysHeld() & KEY_START);
 
-    } while (keysHeld() & KEY_START);
+    /*
+        Hide LCD while main.c changes from Mode 3 to Mode 0.
+    */
+    REG_DISPCNT |= 0x0080;
 }
